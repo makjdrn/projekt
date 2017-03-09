80014004edcb8c2ea048f98a64902a87a6a9595e
makjdrn <belecmarcin@gmail.com>
2017-03-09 13:29:13 +0100

    Dodanie szyfrowania

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, url_for
 from flaskext.mysql import MySQL
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA512
+from Crypto.PublicKey import RSA
+from base64 import b64encode, b64decode
 
 mysql = MySQL()
 app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'makjdrn94'
-app.config['MYSQL_DATABASE_DB'] = 'Test'
+app.config['MYSQL_DATABASE_DB'] = 'Podpisy_Database'
 app.config['MYSQL_DATABASE_host'] = 'localhost'
 mysql.init_app(app)
 
@@ -14,15 +18,23 @@ def index():
     return render_template('index.html')
 @app.route('/verification',methods=['POST', 'GET'])
 def auth():
-    usr = request.form['name']
-    passw = request.form['passw']
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from User where userName ='" + usr + "' and password='" + passw + "'")
-    data = cursor.fetchone()
-    if data is None:
-        return "False"
+    sign = request.form['sign']
+    person = request.form['person']
+    splited = person.split()
+    name = splited[0]
+    surname = splited[1]
+
+    message = open('Test', 'r').read()
+    key = RSA.importKey(open('mypkey.der').read())
+    signature = b64decode(sign)
+    h = SHA512.new(message)
+    verifier = PKCS1_v1_5.new(key)
+    if verifier.verify(h, signature):
+        """ TODO Wyswietlanie dokumentu na ekranie """
+        return render_template('yourein.html')
     else:
-        return render_template('yourein.html')        
+        return "Zle"
+    
 @app.route('/cakes')
 def cakes():
     return 'Yummy cakes!'
